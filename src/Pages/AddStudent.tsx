c3081c6e5fb965f408abe03c8dc82bc6ca25cf97
IrfanShah <71603574+DeveloperIrkhan@users.noreply.github.com>
2021-09-21 12:29:13 +0500

    Validation added and working fine

--- src/Pages/AddStudent.tsx
+++ src/Pages/AddStudent.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import { StudentResponseModel } from '../Models/Students';
-import { loading } from '../Utilities/Gernal-Utilities';
+// import { loading } from '../Utilities/Gernal-Utilities';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import axios from 'axios';
-import PhoneInput from 'react-phone-input-2'
 import 'react-phone-input-2/lib/style.css'
 interface Props { }
 
 interface State {
     Student: StudentResponseModel;
-    validate: boolean
+    notValid: boolean
 
 }
 
@@ -19,7 +18,7 @@ export class AddStudent extends React.Component<Props, State> {
         super(props);
         this.state = {
             Student: Object(),
-            validate: true
+            notValid: false
         };
         this.validationCheck = this.validationCheck.bind(this)
         this.PostStudentdata = this.PostStudentdata.bind(this)
@@ -27,40 +26,48 @@ export class AddStudent extends React.Component<Props, State> {
 
     validationCheck() {
         let Std = this.state.Student;
-        let firstName = Std.firstName;
-        let middlename = Std.middleName;
-        let lastName = Std.lastName;
-        let registerationNo = Std.registerationNo;
-        let phoneNo = Std.phoneNo;
-        let address = Std.address;
-        let department = Std.department;
-        console.log(firstName)
-        console.log(middlename)
-        console.log(lastName)
-        console.log(registerationNo)
-        if (firstName === "" || middlename === "" || lastName === "" || registerationNo === "" || phoneNo === "" || address === "" || department === "") {
-            toast.error("Pleae Fill the required Field!!!")
-            this.setState({ validate: false })
+        if (!(Std.firstName?.length > 0 ||
+            Std.lastName?.length > 0 ||
+            Std.phoneNo?.length > 0 ||
+            Std.registerationNo?.length > 0 ||
+            Std.address?.length > 0 ||
+            Std.department?.length > 0)) {
+            this.setState({ notValid: true }, () => {
+                toast.error("Pleae Fill the required Field!!!")
+
+            })
         }
         else {
-            this.setState({ validate: true })
-            this.PostStudentdata();
-
+            this.setState({ notValid: false }, () => {
+                this.PostStudentdata();
+            })
         }
+
     }
 
 
     async PostStudentdata() {
         try {
             let Student = this.state.Student;
+
             let API_URL = "https://localhost:44378/Student/AddStudent";
-            loading(true);
+            // loading(true);
             await axios.post(API_URL, Student)
-            loading(false);
-            toast.success("Student Details Added Successfully!!!!")
+            Student.firstName = "";
+            Student.middleName = "";
+            Student.lastName = "";
+            Student.phoneNo = "";
+            Student.registerationNo = "";
+            Student.address = "";
+            Student.department = "";
+            // loading(false);
+            this.setState({ Student }, () => {
+                toast.success("Student Details Added Successfully!!!!")
+            })
+
         }
         catch {
-            loading(false);
+            // loading(false);
             toast.error("Error! while adding Student details",
                 {
                     autoClose: 5000,
@@ -80,9 +87,10 @@ export class AddStudent extends React.Component<Props, State> {
     // }
 
     render() {
-        let Save_Student = this.state.Student;
-        // const { firstName, middleName, lastName, registerationNo, phoneNo, address, department } = this.state.Student
-        const { validate } = this.state
+        let Student = this.state.Student;
+        // let 
+        let  notValid  = this.state.notValid;
+        console.log("notValid = ", notValid)
         return (
             <div className="py-5 row col-12">
                 <ToastContainer />
@@ -90,30 +98,33 @@ export class AddStudent extends React.Component<Props, State> {
                     <div className="col-lg-5 col-12 m-auto m-0">
                         <h5 className="text-muted" >Please Enter Your Full Name</h5>
                         <div className="Textbox-div">
-                            <input className={`${Save_Student.firstName === "" && validate !== true ? "RedBorders form-control m-2" : "myInput border-3 form-control m-2"}`}
+                            <input className={`${!(Student.firstName?.length>0) && notValid === true ? "RedBorders form-control shadow-none m-2" : "myInput border-3 form-control shadow-none m-2"}`}
                                 placeholder="Here Your First Name"
                                 type="text"
-                                value={Save_Student.firstName}
+                                value={Student.firstName}
                                 onChange={(e) => {
-                                    Save_Student.firstName = e.target.value
+                                    Student.firstName = e.target.value;
+                                    this.setState({ Student })
                                 }}
                             />
                         </div>
                         <div className="Textbox-div">
-                            <input className={`${Save_Student.middleName === "" && validate !== true ? "RedBorders form-control m-2" : "myInput border-3 form-control m-2"}`}
+                            <input className="myInput border-3 form-control shadow-none m-2"
                                 placeholder="Here Your Middle Name" type="text"
-                                value={Save_Student.middleName}
+                                value={Student.middleName}
                                 onChange={(e) => {
-                                    Save_Student.middleName = e.target.value
+                                    Student.middleName = e.target.value;
+                                    this.setState({ Student })
                                 }}
                             />
                         </div>
                         <div className="Textbox-div">
-                            <input className={`${Save_Student.lastName === "" && validate !== true ? "RedBorders form-control m-2" : "myInput border-3 form-control m-2"}`}
+                            <input className={`${!(Student.lastName?.length>0) && notValid === true ? "RedBorders form-control shadow-none m-2" : "myInput border-3 form-control shadow-none m-2"}`}
                                 placeholder="Here Your Last Name" type="text"
-                                value={Save_Student.lastName}
+                                value={Student.lastName}
                                 onChange={(e) => {
-                                    Save_Student.lastName = e.target.value
+                                    Student.lastName = e.target.value;
+                                    this.setState({ Student })
                                 }}
                             />
                         </div>
@@ -121,46 +132,49 @@ export class AddStudent extends React.Component<Props, State> {
                     <div className="col-lg-5 col-12 m-auto m-0">
                         <h5 className="text-muted" >Please Enter Other's cradincial</h5>
                         <div className="Textbox-div">
-                            <input className={`${Save_Student.registerationNo === "" && validate !== true ? "RedBorders form-control m-2" : "myInput border-3 form-control m-2"}`}
+                            <input className={`${!(Student.registerationNo?.length>0) && notValid === true ? "RedBorders form-control shadow-none m-2" : "myInput border-3 form-control shadow-none m-2"}`}
                                 placeholder="Your Registeration No" type="text"
-                                value={Save_Student.registerationNo}
+                                value={Student.registerationNo}
                                 onChange={(e) => {
-                                    Save_Student.registerationNo = e.target.value
+                                    Student.registerationNo = e.target.value;
+                                    this.setState({ Student })
                                 }}
                             />
                         </div>
                         <div className="Textbox-div">
-                            <input className={`${Save_Student.phoneNo === "" && validate !== true ? "RedBorders form-control m-2" : "myInput border-3 form-control m-2"}`}
+                            <input className={`${!(Student.phoneNo?.length>0) && notValid === true ? "RedBorders form-control shadow-none m-2" : "myInput border-3 form-control shadow-none m-2"}`}
                                 placeholder="Your Phone No" type="text"
-                                value={Save_Student.phoneNo}
+                                value={Student.phoneNo}
                                 onChange={(e) => {
-                                    Save_Student.phoneNo = e.target.value
+                                    Student.phoneNo = e.target.value;
+                                    this.setState({ Student })
                                 }}
                             />
                         </div>
                         <div className="Textbox-div">
-                            <input className={`${Save_Student.address === "" && validate !== true ? "RedBorders form-control m-2" : "myInput border-3 form-control m-2"}`}
+                            <input className={`${!(Student.address?.length>0) && notValid === true ? "RedBorders form-control shadow-none m-2" : "myInput border-3 form-control shadow-none m-2"}`}
                                 placeholder="Your Full Address" type="text"
-                                value={Save_Student.address}
+                                value={Student.address}
                                 onChange={(e) => {
-                                    Save_Student.address = e.target.value
+                                    Student.address = e.target.value;
+                                    this.setState({ Student })
                                 }}
                             />
                         </div>
                         <div className="Textbox-div">
-                            <input className={`${Save_Student.department === "" && validate !== true ? "RedBorders form-control m-2" : "myInput border-3 form-control m-2"}`}
+                            <input className={`${!(Student.department?.length>0) && notValid === true ? "RedBorders form-control shadow-none m-2" : "myInput border-3 form-control shadow-none m-2"}`}
                                 placeholder="Your Department" type="text"
-                                value={Save_Student.department}
+                                value={Student.department}
                                 onChange={(e) => {
-                                    Save_Student.department = e.target.value
+                                    Student.department = e.target.value;
+                                    this.setState({ Student })
                                 }}
                             />
                         </div>
                     </div>
                     <div className="col-lg-5 p-4 col-12 col-md-10  m-auto m-0 col-xm-9">
                         <button
-                            onClick={
-                                this.validationCheck}
+                            onClick={this.validationCheck}
                             className="savebtn col-12">Save
                         </button>
                     </div>
